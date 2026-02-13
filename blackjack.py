c0e96636b56c12bece37e5e0756c804cc2d5c0df
Sobelema <s.atemie@se16.qmul.ac.uk>
2026-02-13 00:05:42 +0000

    application structure & const

---
+++ blackjack.py
@@ -0,0 +1,33 @@
+"""Blackjack
+The classic card game also known as 21. (This version doesn't have splitting or insurance
+Tags: large, game, card game"""
+
+import random, sys
+
+# Set up constants:
+HEARTS = chr(9829)  # Character 9829 is '♥'
+DIAMONDS = chr(9830)    # Character 9830 is '♦'
+SPADES = chr(9824)  # Character 9824 is '♠'
+CLUBS = chr(9827)   # Character 9827 is '♣'
+# A list of chr codes can be found online
+
+def getBet(maxBet):
+    pass
+
+def getDeck():
+    pass
+
+def displayHandss():
+    pass
+
+def getHandValue():
+    pass
+
+def displayCards():
+    pass
+
+def getMove():
+    pass
+
+def main():
+    pass
